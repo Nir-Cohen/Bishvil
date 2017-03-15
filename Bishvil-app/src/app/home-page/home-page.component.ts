aeab209f3e2c498c9e7e83a1307372052b030089
נתנאל מורלי < >
2017-03-15 16:27:24 +0200

    change nav bar

--- Bishvil-app/src/app/home-page/home-page.component.ts
+++ Bishvil-app/src/app/home-page/home-page.component.ts
@@ -7,37 +7,9 @@ import {Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
   
+
   constructor() { }
   
-  ngOnInit() {}
-
-  ngAfterViewChecked() {
-    this.scrollToBottom();
-  }
-
-  scrollToBottom(): void {
-    try {
-      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
-    } catch(err) { }
-  }
-
-  sendMessage(){
-    this.afService.sendMessage(this.newMessage);
-    this.newMessage = '';
-  }
-
-  isYou(email) {
-    if(email == this.afService.email)
-      return true;
-    else
-      return false;
-  }
-
-  isMe(email) {
-    if(email == this.afService.email)
-      return false;
-    else
-      return true;
-  }
+  ngOnInit() { }
 
 }
