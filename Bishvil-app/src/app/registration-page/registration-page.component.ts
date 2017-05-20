ce120f9c459e7a70f4bf082c4ae96a77da2c7d6c
TalYehoshua <talyeh3@gmail.com>
2017-05-20 20:50:36 +0300

    update

--- Bishvil-app/src/app/registration-page/registration-page.component.ts
+++ Bishvil-app/src/app/registration-page/registration-page.component.ts
@@ -13,14 +13,19 @@ userType:Array<Object>  = [
        {id: 1, name: "Bat Sherut"},
        {id: 2, name: "Torem"},
      ];
+     status: number;
 
   constructor(private afService: AF, private router: Router) { }
 
   register(event, name, email, password,status) {
     console.log(status);
+  if(status == "Bat Sherut")
+    this.status=1;
+  else
+    this.status=2;
     event.preventDefault();
     this.afService.registerUser(email, password).then((user) => {//,"1").then((user) => {
-      this.afService.saveUserInfoFromForm(user.uid, name, email,"1").then(() => {
+      this.afService.saveUserInfoFromForm(user.uid, name, email,this.status).then(() => {
         this.router.navigate(['']);
       })
         .catch((error) => {
