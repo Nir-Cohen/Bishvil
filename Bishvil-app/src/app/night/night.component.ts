f6050294e19770a9141aed21caadacb7880fd3ef
TalYehoshua <talyeh3@gmail.com>
2017-06-06 16:16:07 +0300

    jg

--- Bishvil-app/src/app/night/night.component.ts
+++ Bishvil-app/src/app/night/night.component.ts
@@ -17,7 +17,6 @@ import { Confirm2Component } from "app/confirm2/confirm2.component";
 export class NightComponent implements OnInit {
   
   confirmResult:boolean = null;
-<<<<<<< HEAD
   key:string;
   author:string;
   arrusers=[];
@@ -28,42 +27,13 @@ export class NightComponent implements OnInit {
   constructor(private dialogService:DialogService,public afService: AF,public af: AngularFire) { 
   this.events = this.afService.events;
   this.users = this.af.database.list("registeredUsers");
-=======
-
-  constructor(public afService: AF, public af: AngularFire,private dialogService:DialogService) 
-  {
-    this.events = this.afService.event;
-    this.users = this.af.database.list("registeredUsers");
-  }
->>>>>>> db3bc79d0ae4c931c2470bc8dae6c05d3431c2d3
 
   }
-<<<<<<< HEAD
   deleteItem(key : string){
     console.log("Removing "+ key);
     this.events.remove(key);
    }
 
-=======
-
-  leave(key: string) 
-  {
-    this.afService.OK_key = key;
-    var event = this.af.database.object('/events/' + this.afService.OK_key); // How to get value
-    event.subscribe(snapshot => {
-      this.UserAreComing = snapshot.userArr;
-    });
-    var i = 0;
-    for (i = 0; i < this.UserAreComing.length; i++) 
-    {
-      if (this.afService.displayName == this.UserAreComing[i] || this.afService.email == this.UserAreComing[i]) {
-        break;
-      }
-    }
-    this.UserAreComing.splice(i, 1);
-    this.af.database.object('/events/' + this.afService.OK_key).update({ userArr: this.UserAreComing });
-  }
->>>>>>> db3bc79d0ae4c931c2470bc8dae6c05d3431c2d3
 
 
   ngOnInit() {
@@ -79,28 +49,10 @@ leave(key:string)
   var i = 0; 
   for(i = 0 ;i <this.userArr.length;i++)
   {
-<<<<<<< HEAD
     if(this.afService.displayName == this.userArr[i] ||this.afService.email == this.userArr[i])
     {
        break;
     }
-=======
-    var i = 0;
-    var event = this.af.database.object('/events/' + key); // How to get value
-    event.subscribe(snapshot =>
-     {
-      this.comingFromSnap = snapshot.userAreComing;
-    });
-    for(i=0 ;i<this.comingFromSnap.length; i++)
-    {
-      if(this.comingFromSnap[i] == this.afService.email || this.comingFromSnap[i] == this.afService.displayName)
-      {
-        return true;
-      }
-    }            
-    console.log(key);
-    return false;
->>>>>>> db3bc79d0ae4c931c2470bc8dae6c05d3431c2d3
   }
     console.log( this.userArr);
      this.userArr.splice(i,1);
@@ -108,7 +60,6 @@ leave(key:string)
      this.af.database.object('/events/'+this.afService.OK_key).update({userArr : this.userArr});
 }
 
-<<<<<<< HEAD
 
 
   showConfirm(key: string) {
@@ -117,7 +68,7 @@ leave(key:string)
     console.log(this.afService.OK_key);
     this.dialogService.addDialog(Confirm2Component, {
       title:'Confirmation',
-      message:'Are you sure you want to join this Night?'})
+      message:'Are you sure you want to join this Shabbat?'})
       .subscribe((isConfirmed)=>{
         //Get dialog result
         this.confirmResult = isConfirmed;
@@ -159,17 +110,3 @@ leave(key:string)
 
 
 
-=======
- Join(key: string) 
- {
-    this.afService.OK_key=key; 
-    var x  = this.comingFromSnap.fill(this.afService.email);
-    this.af.database.object('/events/'+this.afService.OK_key).update({userAreComing : x});
-    console.log({userAreComing : x});
-  }
-
-  delete(key : string){
-    this.events.remove(key);
-   }
-}
->>>>>>> db3bc79d0ae4c931c2470bc8dae6c05d3431c2d3
